b9df41a0dae7b375b6e81701cbbd2344f0eb685f
Ekakshar Shreyas <shreyasekakshar1605@gmail.com>
2026-06-22 20:10:18 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://stellar-scone-a102ff.netlify.app"
+    ],
   })
 );
 app.use(express.json());
